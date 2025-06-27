4d359a1d2fa6dd8ea45a960489b06aeda8e87bf7
Farooq Ahmed <03174748082ali@gmail.com>
2025-06-27 00:09:15 -0700

    completed

--- app.js
+++ app.js
@@ -1,4 +1,94 @@
-var str = "Hello Javascript";
+// Variables in javacsript
 
-console.log(str);
+//  1 Var
+
+// 2 Let
+
+// 3 Const
+
+
+
+
+// ( Var ) \\
+
+// var str = "Hello Javascript";
+// console.log(str);
+
+// var num = 20;
+// console.log(num);
+
+// var string;
+// console.log(" X Y Z ");
+
+// x = 20;
+// y = 30;
+// z = x + y;
+// console.log(z);
+
+// var num1 = 50;
+// var num2 = 50;
+// var num = num1 + num2;
+// console.log(num);
+
+//var ko re-declare kr sakte hain lakin var ak he chalega
+//(e.g)
+
+// var name = "pakistan";
+// var name = "Pakistan"
+// console.log(name);
+
+
+
+
+// ( Let ) \\
+
+// let str = "Hello Javascript";
+
+// console.log(str);
+
+// let num = 20;
+// console.log(num);
+
+// let string;
+// console.log(" X Y Z ");
+
+// x = 20;
+// y = 30;
+// z = x + y;
+// console.log(z);
+
+// let ko Ek hi scope mein dobara declare nahi kar sakte.
+//(e.g)
+
+// {
+// let age = 20;
+// let age;
+// let age = 35; 
+// console.log(age);
+
+// }
+
+// let ko alag alag scope mein declare kar sakte ho.
+//(e.g)
+
+// {
+//     let age = 20;
+//     console.log(age);
+// }
+
+
+
+// ( Const ) \\
+
+// const str = "Hello Javascript";
+
+// console.log(str);
+
+// const num = 20;
+// console.log(num);
+
+// const string = " X Y Z ";
+// console.log(string);
+
+//cosnt na to redeclare hoga or na he reassign
 
